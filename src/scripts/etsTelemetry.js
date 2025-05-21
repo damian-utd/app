a3c8cc8a7b4437c06985c9bb89dab9724fbc4c08
Xomil <xomilol@gmail.com>
2025-05-21 14:28:07 +0200

    Stoper

--- src/scripts/etsTelemetry.js
+++ src/scripts/etsTelemetry.js
@@ -95,8 +95,8 @@ async function loopFetch() {
     while (true) {
         await sleep(10);
         // const data = await fetchData("http://82.145.73.127:25555/api/ets2/telemetry"); // ZUT
-        // const data = await fetchData("http://192.168.1.11:25555/api/ets2/telemetry"); // DOM
-        const data = await fetchData("http://192.168.56.1:25555/api/ets2/telemetry"); // laptop siec ZUT
+        const data = await fetchData("http://192.168.1.11:25555/api/ets2/telemetry"); // DOM
+        // const data = await fetchData("http://192.168.56.1:25555/api/ets2/telemetry"); // laptop siec ZUT
 
         if (data) {
             wheelUpdate(data.gameSteer, data.userClutch, data.userBrake, data.userThrottle);
---
+++ src/scripts/timer.js
@@ -0,0 +1,46 @@
+window.addEventListener('DOMContentLoaded', () => {
+    let button = document.getElementById('start-stop-button');
+    let timer = document.getElementById('timer-container');
+    button.addEventListener('click', (e) => {
+        if (button.innerText === 'Start') {
+            StartToStop(button, timer);
+        }
+        else if (button.innerText === 'Stop') {
+            StopToStart(button, timer);
+        }
+    })
+})
+
+let interval = null;
+
+function StartToStop(button, timer) {
+    button.innerText = 'Stop';
+    button.style.backgroundColor = '#dc3636';
+
+    let start = Date.now();
+    let time = 0;
+    interval = setInterval(function() {
+        time = Date.now() - start;
+        let minutes = Math.floor(time / 60000);
+        let seconds = Math.floor((time % 60000) / 1000);
+        let centiSeconds = Math.floor((time % 1000) / 10);
+
+        let minStr = minutes.toString().padStart(2, '0');
+        let secStr = seconds.toString().padStart(2, '0');
+        let centiStr = centiSeconds.toString().padStart(2, '0');
+
+        timer.innerText = `${minStr}:${secStr}:${centiStr}`;
+
+    }, 10);
+
+}
+
+function StopToStart(button, timer) {
+    button.innerText = 'Start';
+    button.style.backgroundColor = '#44c767';
+    clearInterval(interval);
+    timer.innerText = '00:00:00'
+}
+
+
+
